56f300541751df088b49531e8c1ee7f915c08d76
michimichi2021 <michikaho1009@gmail.com>
2022-12-30 16:49:28 +0900

    マークアップエディタ見本

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {marked} from "marked";
+import sanitizeHtml from 'sanitize-html';
+
 
 function App() {
+  const [markdown, setMarkdown] = useState('');
+  const markedText = sanitizeHtml(markdown, {
+    allowedTags: [],
+    disallowedTagsMode: 'recursiveEscape',
+  });
+
+  marked.setOptions({
+    gfm: true,
+    breaks: true,
+  });
+
+  const htmlText = marked.parse(markedText);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <form>
+          <label>
+            メッセージ
+          </label><br/>
+          <textarea name="name" rows={10} cols={40} onChange={(e) => setMarkdown(e.target.value)}/>
+        </form>
+      </div>
+      <div>
+        <p>プレビュー</p>
+        <div className='bg-gray-200 p-3 text-sm w-full prose prose-sm'>
+          <div dangerouslySetInnerHTML={{ __html: htmlText }} className='w-3/4' />
+        </div>
+      </div>
+    </>
   );
 }
 
